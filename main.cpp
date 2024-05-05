9b64959e6b9cc1274fe991708a2d480d2eafe464
LuckySubham <subhsinging@gmail.com>
2024-05-06 01:17:17 +0530

    v

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int age;
+        cout << "enter age" << endl;
+        cin >> age;
+
+        cout << "your age is" << endl;
+
+printf("Hello Duniya");
+
+        return 0;
+
+}
+
+       
