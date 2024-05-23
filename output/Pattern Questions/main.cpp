fadc3762db01a683c678e492bd970f6262648a81
LuckySubham <subhsinging@gmail.com>
2024-05-24 01:44:00 +0530

    Week 2 final pattern class

--- output/Pattern Questions/main.cpp
+++ output/Pattern Questions/main.cpp
@@ -310,3 +310,218 @@ using namespace std;
 //     }
 // }
 
+// 14 - Printing Inverted Hollow Pyramid   --   do it
+// ==============================================================================
+
+// int main(){
+
+//     int n=8;
+//     for(int row=0; row<n; row++){
+
+//         for(int col=0; col<row; col++)
+//             cout<< " ";
+
+//         for(int col=0; col<n-row; col++){
+
+//             if( row ==0 || row == n-1 || col==0 || col ==n-row-1)
+
+//                 cout<<"* ";
+//             else
+//                 cout<< "  ";
+//         }
+
+//         cout<< endl;
+
+//     }
+// }
+
+// 15 - Printing  Hollow Diamond   --   do it
+// ==============================================================================
+
+// void fullHollowPyramid(int n){
+
+//     for (int row = 0; row < n; row++)
+//     {
+
+//         for (int col = 0; col < n - row - 1; col++)
+
+//             cout << " ";
+
+//         for (int col = 0; col < row + 1; col++)
+//         {
+
+//             if (col == 0 || row == 0 || /* row == n - 1  || */ col == row)
+//                 cout << "* ";
+//             else
+//                 cout << "  ";
+//         }
+
+//         cout << endl;
+//     }
+
+// }
+
+// void invertedHollowFullPyramid(int n){
+
+//     for(int row=0; row<n; row++){
+
+//         for(int col=0; col<row; col++)
+//             cout<< " ";
+
+//         for(int col=0; col<n-row; col++){
+
+//             if( /* row ==0 || */row == n-1 || col==0 || col ==n-row-1)
+
+//                 cout<<"* ";
+//             else
+//                 cout<< "  ";
+//         }
+
+//         cout<< endl;
+
+//     }
+// }
+
+// void diamond(int n){
+//         fullHollowPyramid(n);
+//         invertedHollowFullPyramid(n);
+
+// }
+
+// int main(){
+
+// int n=5;
+//     diamond(n);
+
+// }
+
+// 15 - Printing  Mixed Pyramid and Diamond   --   do it
+// ==============================================================================
+// * * * * *   * * * * *
+// * * * *       * * * *
+// * * *           * * *
+// * *               * *
+// *                   *
+// *                   *
+// * *               * *
+// * * *           * * *
+// * * * *       * * * *
+// * * * * *   * * * * *
+// int main(){
+
+//     int n=5;
+
+//     for(int row =0; row<n; row++){
+
+//         // Upper Part
+//         for(int col=0; col<n-row; col++)
+//             cout<< "* ";
+
+//         for(int col=0; col<2*row+1; col++)
+//             cout<< "  ";
+
+//         for(int col=0; col<n-row; col++)
+//             cout<< "* ";
+//         cout<< endl;
+
+//     }
+
+//     for(int row =0; row<n; row++){
+
+//         // Lower Part
+//         for(int col=0; col<row+1; col++)
+//             cout<< "* ";
+
+//         for(int col=0; col<2*(n-row)-1; col++)
+//             cout<< "  ";
+
+//         for(int col=0; col<row+1; col++)
+//             cout<< "* ";
+//         cout<< endl;
+
+//     }
+// }
+
+// 16 - Printing  FANCY 12345  pattern--   do it
+// ==============================================================================
+
+// 1
+// 2*2
+// 3*3*3
+// 4*4*4*4
+// 5*5*5*5*5
+
+// int main(){
+
+//     int n=5;
+
+//     for(int row=0; row<n; row++){
+
+//         for(int col=0; col<2*row+1; col ++){
+//             if(col % 2==0){
+//                 cout<< row+1;
+//             }else{
+//                 cout<< "*";
+//             }
+//         }
+//          cout<< endl;
+//     }
+// }
+
+// 17 - Printing  FANCY 12345--54321  pattern--   do it
+// ==============================================================================
+
+// int main(){
+
+//     int n=5;
+
+//     for(int row=0; row<n; row++){
+
+//         for(int col=0; col<2*row+1; col ++){
+//             if(col % 2==0){
+//                 cout<< row+1;
+//             }else{
+//                 cout<< "*";
+//             }
+//         }
+//          cout<< endl;
+//     }
+
+// n--;
+//     for(int row=0; row<n; row++){
+
+//         for(int col=0; col<2*(n-row)-1;col++){
+//             if(col % 2 ==0){
+//                 cout<< n-row;
+//             }else{
+//                 cout<< "*";
+//             }
+//         }
+//         cout<< endl;
+//     }
+// }
+
+// 17 - Printing  FANCY 12345--54321  pattern--   do it - very Important do it.
+// ==============================================================================
+
+// int main(){
+
+//     int n=5;
+//     for(int row=0; row<n; row++){
+
+//         char ch='A';
+//         for(int col =0; col< row+1; col++){
+//             cout<<ch;
+//             ch+=1;
+//         }
+//         ch--;
+
+//         while(ch > 'A'){
+
+//             ch--;
+//             cout<< ch;
+//         }
+
+//         cout<< endl;
+//     }
+// }
